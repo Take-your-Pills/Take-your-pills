7aee8fd48b399350f9633058b82d6c3b99665341
François ARBOGAST <arbogast.francois@gmail.com>
2020-06-24 16:45:13 +0100

    Form in progress

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -1,5 +1,38 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
 const CreateAccount = () => {
-  return <div></div>;
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Enter your name"
+          ref={register({ required: "Your name is required" })}
+        />
+        <input
+          name="birthdate"
+          type="date"
+          placeholder="Enter your Birthday"
+          ref={register({ required: "Your Birthday is required" })}
+        />
+        <select
+          name="gender"
+          ref={register({ required: "Your gender is required" })}
+        >
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+      </form>
+    </div>
+  );
 };
 
 export default CreateAccount;
